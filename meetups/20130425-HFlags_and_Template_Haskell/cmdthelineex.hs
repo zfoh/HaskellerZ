415165a3ac9cb7db115ef0b3a46e06f4f2398dde
Gergely Risko <gergely@risko.hu>
2013-05-05 11:22:57 +0200

    Update the TH presentation

--- meetups/20130425-HFlags_and_Template_Haskell/cmdthelineex.hs
+++ meetups/20130425-HFlags_and_Template_Haskell/cmdthelineex.hs
@@ -9,10 +9,14 @@ name = value $ opt "Indiana Jones" $ optInfo [ "name", "n" ]
 times :: Term Int
 times = value $ opt 4 $ optInfo [ "times", "t" ]
 
+hello :: String -> Int -> IO ()
 hello name times = sequence_ $ replicate times $ putStrLn name
 
+term :: Term (IO ())
 term = hello <$> name <*> times
 
+termInfo :: TermInfo
 termInfo = defTI { termName = "Hello", version = "1.0" }
 
+main :: IO ()
 main = run ( term, termInfo )
