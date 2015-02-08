0f9d7f6f611792d63dd47a8e3482b97f2e691196
Simon Meier <simon.y.meier@gmail.com>
2015-02-08 17:45:02 +0100

    Fix some more issues in demo program.

--- meetups/2015-02-26-Writing_your_first_real_world_Haskell_application/wc/src/Main.hs
+++ meetups/2015-02-26-Writing_your_first_real_world_Haskell_application/wc/src/Main.hs
@@ -16,6 +16,7 @@ import qualified Data.ByteString              as B
 import qualified Data.Text                    as T
 import qualified Data.Text.Encoding           as TE
 
+import           System.Directory             (doesFileExist)
 import           System.Environment           (getArgs)
 
 
@@ -30,22 +31,31 @@ main = do
       ("-m" : args) -> use (T.length . TE.decodeUtf8)         args
       ("-l" : args) -> use (length . T.lines . TE.decodeUtf8) args
       ("-w" : args) -> use (length . T.words . TE.decodeUtf8) args
-      (flag : _   ) -> putStrLn $ "unsupported flag: " ++ flag
+      (flag : _   ) -> putStrLn $ "wc: unsupported flag '" ++ flag ++ "'."
 
-
+-- | Summarize a number of files using the given function.
+--
+-- (This is a ♥ to have a multi-byte character in our source code.)
 use :: Show a => (B.ByteString -> a) -> [FilePath] -> IO ()
 use summarizeBytes fileNames =
     forM_ fileNames $ \fileName -> do
-        --
-        -- Note that we are reading the whole file content into memory. This
-        -- is definitely inefficient and unnecessary. For smallish files this
-        -- is however totally OK, and it allows us to demonstrate the
-        -- composition of a Haskell applications from a pure core and an IO
-        -- layer.
-        --
-        -- For proper streaming implementations with the same compositional
-        -- structure one would use the `conduit` or `pipes` libraries. Don't
-        -- use lazy IO except you really know what you are doing!
-        --
-        content <- B.readFile fileName
-        return (summarizeBytes content)
+        isFile  <- doesFileExist fileName
+        summary <-
+          if not isFile
+            then return "not a file"
+            else do
+              --
+              -- Note that we are reading the whole file content into memory. This
+              -- is definitely inefficient and unnecessary. For smallish files this
+              -- is however totally OK, and it allows us to demonstrate the
+              -- composition of a Haskell applications from a pure core and an IO
+              -- layer.
+              --
+              -- For proper streaming implementations with the same compositional
+              -- structure one would use the `conduit` or `pipes` libraries. Don't
+              -- use lazy IO except you really know what you are doing!
+              --
+              content <- B.readFile fileName
+              return (show $ summarizeBytes content)
+
+        putStrLn $ fileName ++ "   " ++ summary
