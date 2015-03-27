eb21f5ffbecec5dba3750573ee96e11024b3db73
Mihaly Barasz <klao@nilcons.com>
2015-03-27 16:39:13 +0100

    demo-game: Step 3
    Handling events and saving the pressed keys in the state.

--- meetups/2015-03-26-Writing_Games_in_Haskell/demo-game/src/Main.hs
+++ meetups/2015-03-26-Writing_Games_in_Haskell/demo-game/src/Main.hs
@@ -3,20 +3,25 @@
 
 module Main (main) where
 
-import Graphics.Gloss
 import Control.Lens
 import Data.Default
+import Graphics.Gloss
+import Graphics.Gloss.Interface.Pure.Game
+import Control.Arrow
+import Data.Monoid
 
 import Player
+import Keys
 
 data World
   = World
     { _wPlayer :: !Player
+    , _wKeys :: !KeySet
     } deriving (Show)
 makeLenses ''World
 
 iniWorld :: World
-iniWorld = World def
+iniWorld = World def mempty
 
 drawWorld :: World -> Picture
 drawWorld World{..} = drawPlayer _wPlayer
@@ -24,7 +29,13 @@ drawWorld World{..} = drawPlayer _wPlayer
 stepWorld :: Float -> World -> World
 stepWorld dt = wPlayer %~ stepPlayer dt
 
+handleEvents :: Event -> World -> World
+handleEvents ev = wKeys %~ updateKeys ev >>> updatePlayers
+  where
+    updatePlayers w@World{..} =
+      w & wPlayer %~ updatePlayerByKeys (activeForId 0 _wKeys)
+
 main :: IO ()
-main = play window white 60 iniWorld drawWorld (const id) stepWorld
+main = play window white 60 iniWorld drawWorld handleEvents stepWorld
   where
     window = InWindow "Demo Game" (1200,800) (0,0)
--- meetups/2015-03-26-Writing_Games_in_Haskell/demo-game/src/Player.hs
+++ meetups/2015-03-26-Writing_Games_in_Haskell/demo-game/src/Player.hs
@@ -3,9 +3,13 @@
 
 module Player where
 
-import Graphics.Gloss
 import Control.Lens
+import Data.AffineSpace
 import Data.Default
+import Data.VectorSpace
+import Graphics.Gloss
+
+import Keys
 
 playerR :: Float
 playerR = 30
@@ -13,11 +17,12 @@ playerR = 30
 data Player
   = Player
     { _pPos :: !Point
+    , _pSpd :: !(Float, Float)
     } deriving (Show)
 makeLenses ''Player
 
 instance Default Player where
-  def = Player (0,0)
+  def = Player (0,0) (0,0)
 
 drawPlayer :: Player -> Picture
 drawPlayer Player{..} = translateP _pPos $ circleSolid playerR
@@ -27,4 +32,15 @@ translateP (x,y) = translate x y
 
 
 stepPlayer :: Float -> Player -> Player
-stepPlayer dt = pPos._1 +~ 50*dt
+stepPlayer dt p@Player{..} = p & pPos %~ (.+^ (dt *^_pSpd))
+
+
+updatePlayerByKeys :: ActiveKeys -> Player -> Player
+updatePlayerByKeys (ActiveKeys left right down) = pSpd .~ spd
+  where
+    spd = 50 * (horiz ^+^ vert)
+    vert = if down then (0,-1) else (0,0)
+    horiz = case (left,right) of
+      (True, False) -> (-1,0)
+      (False, True) -> (1,0)
+      _ -> (0,0)
